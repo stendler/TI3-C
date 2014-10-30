9aca91b9b558ae565f76cad3c3190d6967440bd5
stendler <m.stendler@fu-berlin.de>
2014-10-30 13:23:37 +0100

    compile.c manuelles append von .c an den Dateinamen  Known Bugs: pointer to int cast  NOT WOKRING

--- compile.c
+++ compile.c
@@ -5,9 +5,27 @@
 int main()
 {
 	char filename[20];
-	scanf("Dateiname %s",&filename);
+	printf("Dateiname: ");
+	scanf("%s",filename);
 	char call[80] = "gcc -std=c99 -Wall -pedantic -o";
-	char filenamec[22] = strcat(filename, ".c");
-	printf("%s %s %s",call,filenamec,filename);
+	char filenamec[22];
+	//filenamec = strcat(filename, ".c");
+	int i = 0;
+	for(int i = 0; i <= 20; i++){
+		if(filename[i] != 127 && filename[i] != 0){
+			filenamec[i] = filename[i];
+			//DEBUG
+			printf("%d %d %c \n",i,filenamec[i],filenamec[i]);
+		}else{
+			filenamec[i] = (char)".";
+			printf("%d %d %c \n",i,filenamec[i],filenamec[i]);
+			filenamec[i+1] = (char)'c';
+			printf("%d %d %c \n",i,filenamec[i],filenamec[i]);
+			filenamec[i+2] = filename[i];
+			printf("%d %d %c \n",i,filenamec[i],filenamec[i]);
+			i+=2;
+		}
+	}
+	printf("%s %s %s",*call,*filename,*filenamec);
 	return 0;
 }

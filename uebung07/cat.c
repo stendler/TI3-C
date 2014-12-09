3d2aab468c7509c9a83730dfff43e9c35df0aad5
stendler <m.stendler@fu-berlin.de>
2014-12-09 19:25:30 +0100

    Grundfunktionen Ansaetze + dateinamen finden (argv oder stdin) + datei oeffnen + dateistream lesen + fehlermeldung + dateistream schliessen

--- uebung07/cat.c
+++ uebung07/cat.c
@@ -1,4 +1,4 @@
-//TI3 - Uebung 05
+//TI3 - Uebung 07
 //Tutor: Thomas
 //Bearbeiter: Jasmine Cavael und Maximilian Stendler
 
@@ -9,19 +9,27 @@ den Inhalt dieser Datei ausgibt.
 
 #include <stdio.h>
 
-int main(int argc, , char *argv[]){
+int main(int argc, char *argv[]){
   //Anzahl der Parameter ueberpruefen
+  char* inputfile = "";
   if(argc > 1){ //Parameter wurde direkt uebergeben
-    //datei oeffnen
-
-    //ueberpruefen, ob valide
-
-    //datei blockweise einlesen
-    //datei blockweise in stdout schreiben
-  }else{
-    //ueberpruefen, ob dateiname evtl in stdin?
-    //like above
-
-    //or error - stderr
+    inputfile = argv[1];
+  }else{ //ueberpruefen, ob dateiname evtl in stdin?
+    fread(inputfile,128,1,stdin);
   }
+  //datei oeffnen
+  void* filestream = fopen(inputfile,"r");
+  //ueberpruefen, ob valide
+  if(filestream){
+    //datei blockweise einlesen
+    //char* buffer[1024];
+    while(fread(stdout,1024,1,filestream)){
+    //datei blockweise in stdout schreiben
+      //fwrite(stdout,1024,1,buffer);
+    }
+  }else{
+    //or error - stderr
+    fwrite("File not found.",128,1,stderr);
+  }
+  fclose(filestream);
 }
--- uebung07/wc.c
+++ uebung07/wc.c
@@ -11,23 +11,31 @@ die Anzahl der Zeilen, Worten und Bytes dieser Datei ausgibt.
 
 int main(int argc, , char *argv[]){
   //Anzahl der Parameter ueberpruefen
+  char* inputfile = "";
   if(argc > 1){ //Parameter wurde direkt uebergeben
-    //datei oeffnen
-
-    //ueberpruefen, ob valide
-
+    inputfile = argv[1];
+  }else{ //ueberpruefen, ob dateiname evtl in stdin?
+    fread(inputfile,128,1,stdin);
+  }
+  //datei oeffnen
+  void* filestream = fopen(inputfile,"r");
+  //ueberpruefen, ob valide
+  if(filestream){
+    //datei blockweise einlesen
+    //char* buffer[1024];
+    while(fread(stdout,1024,1,filestream)){
+      //datei blockweise in stdout schreiben
+      //fwrite(stdout,1024,1,buffer);
+    }
     //datei zeichenweise einlesen
-    //fuer jedes zeichen byte zaehler incr
-    //fuer jedes leerzeichen wort zaehler incr
-    //fuer jedes (umm enter oder \0 ??) zeilen zaehler incr
 
+    //fuer jedes zeichen byte zaehler incr fgetc
+    //fuer jedes leerzeichen wort zaehler incr
+    //fuer jedes (umm enter oder \0 ??) zeilen zaehler incr fgets
     //am ende der datei -> Zaehler ausgeben
   }else{
-    //stdin checken
-
-    //same
-
-    //bzw error
+    //error- stderr
+    fwrite("File not found.",128,1,stderr);
   }
-
+  fclose(filestream);
 }

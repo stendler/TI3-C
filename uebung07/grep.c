0ed56fa85c238fa33bafea7bc41749e23727d886
stendler <m.stendler@fu-berlin.de>
2014-12-11 08:02:19 +0100

    grep.c: Grundfunktion ueber Parameter mit strstr   + ueberprueft, ob min 2 Parameter (string&file) vorhanden   + oeffnet file und gibt alle Zeilen aus, in denen der string vorkommt

--- uebung07/grep.c
+++ uebung07/grep.c
@@ -8,23 +8,39 @@ rameter erhaelt und alle Zeilen aus der Datei ausgibt, die diesen String enthalt
 */
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, , char *argv[]){
+int main(int argc, char *argv[]){
+  void* filestream;
+  char* inputstring;
   //Anzahl der Parameter ueberpruefen
-  if(argc > 2){ //Parameter wurde direkt uebergeben
-    //ist eingabestring leer?
-    //datei oeffnen
-
-    //ueberpruefen, ob valide
-
+  if(argc > 1){
+    inputstring = argv[1];
+    if(argc > 2){ //Parameter wurde direkt uebergeben
+      filestream = fopen(argv[2],"r");
+    }else{ //ueberpruefen, ob dateiname evtl in stdin?  //TODO: Soll denn ueberhaupt der Dateiname hierdrin sein? Oder bereits die ganze Datei?
+      char inputfile[128] = "";
+      fgets(inputfile,sizeof(inputfile),stdin);
+      //DEBUG
+      printf("%s\n",inputfile);
+      filestream = fopen(inputfile,"r");
+    }
+  }
+  //ueberpruefen, ob valide
+  if(filestream){
     //liest datei zeilenweise ein
+    char buffer[1024];
+    while(fgets(buffer,1024,filestream)){
     //ueberprueft, ob eingabe string innerhalb der Zeile
+      if(strstr(buffer,inputstring)){ //smthng like elem(buffer,inputstring); | find(buffer,inputstring);
       //falls ja zeile in stdout schreiben
-
+      fputs(buffer,stdout);
+      }
+    }
   }else{
     //parameter in stdin zu finden?
-      //same as above
-
     //else error
+    fwrite("File not found.\n",128,1,stderr);
   }
-}
+    return 0;
+  }

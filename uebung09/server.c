fa4a256701195cc152cf17dabec08c3ea76c878d
stendler <m.stendler@fu-berlin.de>
2015-01-08 08:16:54 +0100

    u9 v1.1 Socket check + comments

--- uebung09/server.c
+++ uebung09/server.c
@@ -4,16 +4,25 @@
 //TI3 - Uebung 09
 //Tutor: Thomas
 //Bearbeiter: Jasmine Cavael und Maximilian Stendler
-// Ver 1.0
+// Ver 1.1
 //main() - creates a tcp/ip4 socket
 int main(int arc, char *argv[])
 {
+
+  // 1 Parameter (Portnr.)
     int socket_descriptor;
     socket_descriptor = socket(
       AF_INET,     // Address Family - AF_INET (this is IP version 4)
       SOCK_STREAM, // Type           - SOCK_STREAM (this means connection oriented TCP protocol)
       0            // Protocol       - 0 [ or IPPROTO_IP This is IP protocol]
     );
+    //Type - SOCK_DGRAM - UDP
+
+    // check if socket got successfully created
+    if(socket_descriptor == -1){
+      //failed to create socket
+
+    }
 
     return 0; // end of program
 }

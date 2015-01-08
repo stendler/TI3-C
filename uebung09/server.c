3d5249fbbf2a83f63fb1267ca921e5f58fa8e5dc
stendler <m.stendler@fu-berlin.de>
2015-01-08 09:08:16 +0100

    u9 v1.2 sockaddr_in & bind

--- uebung09/server.c
+++ uebung09/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>	//inet_addr
 
 //TI3 - Uebung 09
 //Tutor: Thomas
@@ -11,18 +12,20 @@ int main(int arc, char *argv[])
   // 1 Parameter (Portnr.)
   if(argc < 2){
     fputs("No Parameter. At least Portnumber needed.",stderr);
-    return -1;
+    return 1;
   }else{
 
     int port;
     int socket_descriptor;
+    struct sockaddr_in server, client;
+    char client_message[2000];
 
     //read Parameter
     port = argv[1]; //string to int?!?!
 
     //create socket
     socket_descriptor = socket(
-      AF_INET,     // Address Family - AF_INET (this is IP version 4)
+      AF_INET,     // Address Family - AF_INET (this is IP version 4) - AF-INET6 IPv6 - AF_UNSPEC
       SOCK_STREAM, // Type           - SOCK_STREAM (this means connection oriented TCP protocol)
       0            // Protocol       - 0 [ or IPPROTO_IP This is IP protocol]
     );
@@ -32,8 +35,25 @@ int main(int arc, char *argv[])
     if(socket_descriptor == -1){
       //failed to create socket
       fputs("Failed to create socket.",stderr);
-      return -2;
+      return 2;
+    }else{
+      fputs("Socket created.",stdout);
     }
+
+    //sockaddr_in
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    //Bind
+    if(bind(socket_descriptor,(struct sockaddr *)&server,sizeof(server)) < 0){
+      fputs("Failed to bind.",stderr);
+      //alternative: perror("Failed to bind."); ?!
+      return 3;
+    }else{
+      fputs("Bind - check.",stdout);
+    }
+    
 
     return 0; // end of program
   }

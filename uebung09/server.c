e76dafdc24f1ccf3ba8d4ee3e7063d0ba3699ae5
stendler <m.stendler@fu-berlin.de>
2015-01-08 08:30:19 +0100

    u9  v1.2 Parameter and errors l. 21 string to int conversion?

--- uebung09/server.c
+++ uebung09/server.c
@@ -4,13 +4,23 @@
 //TI3 - Uebung 09
 //Tutor: Thomas
 //Bearbeiter: Jasmine Cavael und Maximilian Stendler
-// Ver 1.1
+// Ver 1.2
 //main() - creates a tcp/ip4 socket
 int main(int arc, char *argv[])
 {
-
   // 1 Parameter (Portnr.)
+  if(argc < 2){
+    fputs("No Parameter. At least Portnumber needed.",stderr);
+    return -1;
+  }else{
+
+    int port;
     int socket_descriptor;
+
+    //read Parameter
+    port = argv[1]; //string to int?!?!
+
+    //create socket
     socket_descriptor = socket(
       AF_INET,     // Address Family - AF_INET (this is IP version 4)
       SOCK_STREAM, // Type           - SOCK_STREAM (this means connection oriented TCP protocol)
@@ -21,8 +31,10 @@ int main(int arc, char *argv[])
     // check if socket got successfully created
     if(socket_descriptor == -1){
       //failed to create socket
-
+      fputs("Failed to create socket.",stderr);
+      return -2;
     }
 
     return 0; // end of program
+  }
 }

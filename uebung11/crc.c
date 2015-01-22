649759eb4116a3b1858d4854cf63b833e0c0fad9
stendler <m.stendler@fu-berlin.de>
2015-01-22 07:56:32 +0100

    u11: .crc -- Dateiendung

--- uebung11/crc.c
+++ uebung11/crc.c
@@ -11,13 +11,35 @@ int main(int argc, char *argv[])
 {
   if(argc == 2){ //argumente ueberpruefen
     char *divident[2];
-    //.crc endung? --> letztes vorkommen eines . (wenn ueberhaupt) und die naechsten 3 Zeichen ueberpruefen
-    if((strstr(argv[1],".crc")) != NULL){
-      //check checksum
-    }else{
-      //create checksum
+    //divident[0] =
+    //divident[1] =
+
+    //.crc endung? --> letzte 4 Zeichen auf .crc ueberpruefen
+    /*for(int i = 0; i<10;i++){printf("%c %d\n",argv[1][i],argv[1][i]);   fflush(stdout);    }*/ //string terminiert bei 0 - wer haette es gedacht..
+    int len = 0;
+    while(argv[1][len] != 0){
+      len++;
     }
+    if(
+      argv[1][len-3] == '.' &&
+      argv[1][len-2] == 'c' &&
+      argv[1][len-1] == 'r' &&
+      argv[1][len] == 'c'
+    ){
+      //check checksum
+
+    }else{
+        //create checksum
+        //dateiinhalt um 2 leere (16x0) bytes erweitern
+        //immer 2 bytes einlesen und durch divident teilen (mod 2) / bzw xor
+
+    }
+
+    //if((strstr(argv[1],".crc")) != NULL){
+
+    //}else{
+    //}
   }else{
-    printf("usage: crc <filename>");
+    printf("usage: %s <filename>\n",argv[0]);
   }
 }

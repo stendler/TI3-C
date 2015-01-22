ea3acbb2225e99ce87a13e556ce8ba0ac3d69c0c
stendler <m.stendler@fu-berlin.de>
2015-01-22 12:32:18 +0100

    u11: hilsfunktionen fuer bitfolgen

--- uebung11/crc.c
+++ uebung11/crc.c
@@ -10,79 +10,17 @@
 #define DECODE 0
 #define ENCODE 1
 
+int MODE;
+
 void byteToBits(char *bits[16], char byte, char byte2){
   //bits:   jedes element dieses arrays[16] stellt ein bit dar
   //byte:   byte das in eine bitfolge konvertiert werden soll
   unsigned char helpbyte = (unsigned char)byte;
-/*  if(helpbyte >= 128){
-    helpbyte -= 128;
-    bits[0] = 1;
-  }
-  if(helpbyte >= 64){
-    helpbyte -= 64;
-    bits[1] = 1;
-  }
-  if(helpbyte >= 32){
-    helpbyte -= 32;
-    bits[2] = 1;
-  }
-  if(helpbyte >= 16){
-    helpbyte -= 16;
-    bits[3] = 1;
-  }
-  if(helpbyte >= 8){
-    helpbyte -= 8;
-    bits[4] = 1;
-  }
-  if(helpbyte >= 4){
-    helpbyte -= 4;
-    bits[5] = 1;
-  }
-  if(helpbyte >= 2){
-    helpbyte -= 2;
-    bits[6] = 1;
-  }
-  if(helpbyte >= 1){
-    helpbyte -= 1;
-    bits[7] = 1;
-  }
-  helpbyte = (unsigned char)byte2;
-  if(helpbyte >= 128){
-    helpbyte -= 128;
-    bits[8] = 1;
-  }
-  if(helpbyte >= 64){
-    helpbyte -= 64;
-    bits[9] = 1;
-  }
-  if(helpbyte >= 32){
-    helpbyte -= 32;
-    bits[10] = 1;
-  }
-  if(helpbyte >= 16){
-    helpbyte -= 16;
-    bits[11] = 1;
-  }
-  if(helpbyte >= 8){
-    helpbyte -= 8;
-    bits[12] = 1;
-  }
-  if(helpbyte >= 4){
-    helpbyte -= 4;
-    bits[13] = 1;
-  }
-  if(helpbyte >= 2){
-    helpbyte -= 2;
-    bits[14] = 1;
-  }
-  if(helpbyte >= 1){
-    helpbyte -= 1;
-    bits[15] = 1;
-  }*/
   char c = 128;
   for(int i = 0;i<16;i++){
     if(c == 1){
       c = 128;
+      helpbyte = (unsigned char)byte2;
     }
     if(helpbyte >= c){
       helpbyte -= c;
@@ -92,12 +30,37 @@ void byteToBits(char *bits[16], char byte, char byte2){
   }
 }
 
+void refillBits(char *bits[16]){
+    memset(&bits,0,sizeof bits);
+    byteToBit(bits,fgetc(fp),fgetc(fp));
+}
+
+void fillBits(char *bits[16],char *bits2[16]){
+  int i = 0;
+  //count how many leading zeros bits has
+  while(bits[i] == 0){
+    i++;
+  }
+  int shift = i;
+  //shift the first 1 to the most left
+  //&& take the rest from bits2
+  while(i<16){
+    bits[i-shift] = bits[i];
+    if(bits2[i-shift] == -1){
+      //refill
+    }
+    bits[i] = bits2[i-shift]
+    //shift bits2
+    bits2[i-shift] = -1;
+    i++;
+  }
+}
 
 
 int main(int argc, char *argv[])
 {
   if(argc == 2){ //argumente ueberpruefen
-    char mode;
+    //char mode;
     unsigned char divident[2]; //CRC16 = x 16 + x 15 + x 2 + 1
     // 1100 0000  0000 0101
     divident[0] = 192;
@@ -118,18 +81,18 @@ int main(int argc, char *argv[])
         argv[1][len] == 'c'
       ){
         //check checksum
-        mode = DECODE;
+        MODE = DECODE;
       }else{
         //create checksum
         //dateiinhalt um 2 leere (16x0) bytes erweitern
         //immer 2 bytes einlesen und durch divident teilen (mod 2) / bzw xor
-        mode = ENCODE;
+        MODE = ENCODE;
       }
       //neuer dateiname
       char filename[128];
       memset(filename,0,sizeof filename);
       filename = argv[1];
-      if(mode){ //ENCODE
+      if(MODE){ //ENCODE
         filename[len-3] = '.';
         filename[len-2] = 'c';
         filename[len-1] = 'r';
@@ -140,23 +103,8 @@ int main(int argc, char *argv[])
       //create file
       FILE *outputf = fopen(filename,"w");
 
-      char bits[16];
-      unsigned char input[2];
-      input[0] = (unsigned char)fgetc(fp);
-      if(input[0] == EOF){ //ende der datei erreicht beim ersten eingelesenen byte
-        if(mode){
-          input[0] = 0;
-          input[1] = 0;
-        }else{
-          //error - datei zu klein
-        }
-      }else{
-          input[1] = (unsigned char)fgetc(fp);
-          if(input[1] == EOF){
-            input[1]
-          }
-        }
-      }
+      char bits[16], queue[16];
+
     }else{
       printf("File not found\n");
     }

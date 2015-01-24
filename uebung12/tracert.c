a7b20073fbb81d62b4d290ac0c813c9515a2ae0f
stendler <m.stendler@fu-berlin.de>
2015-01-24 23:52:10 +0100

    basic construct

--- uebung12/tracert.c
+++ uebung12/tracert.c
@@ -10,17 +10,41 @@ Traceroute: Traceroute ist ein Programm, welches eine Route bis zu einem gewuens
             vom ersten Router, der dieses Paket bekommt verworfen wird. Der Router schickt eine Fehlermeldung zurueck.
             Dies wird jeweils mit einem inkrementierten TTL wiederholt, bis der gewuenschte Host erreicht wird.
             Alle Router, die dabei einen Fehler gemeldet haben, bilden dann eine Route.
+            https://en.wikipedia.org/wiki/Traceroute
 
 Time-To-Live(TTL): TTL in IP Paketen bezeichnet die uebrige Lebensdauer eines Paketes bis es verworfen wird.
                   Jeder Hop, der dieses Paket erhaelt dekrementiert den TTL um 1 und verwirft das Paket, wenn der TTL 0 erreicht.
                   Zusaetzlich wird ein Fehler dem urspruenglichen Sender des Pakets uebermittelt.
                   Diese Methode wird verwende, damit Pakete, die ihr Ziel nicht finden, nicht das Netzwerk fluten können.
+                  https://en.wikipedia.org/wiki/Time_to_live
 
 Internet Control Message Protocol (ICMP): Das ICMP wird von Netzwerk Geraeten dazu verwendet, um Fehlerberichte zu uebermitteln.
             Sollte z.B. der TTL eines IP Pakets 0 erreichen, dann wird der Hop einen Fehler mittels ICMP versenden.
             In dem 8-Byte Header vom ICMP wird unter anderem der Typ des Fehlers angegeben.
+            https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol
 
 Raw-Socket: Raw-Sockets sind Internet-Sockets, die nicht wie Standart-Sockets auf dem Transport Layer aufsetzen, sondern
             einen Layer tiefer Raw-Packets inklusive header senden und empfangen koennen.
 
 */
+
+
+//includes
+
+//main
+
+//check parameter -> hostname / ip-address
+
+//create send socket
+
+//create listener socket
+
+//unsigned char ttl = 1;
+//while loop
+
+//send custom ip header with udp header in body and ttl
+//multiple simultanously ?
+
+//receive ICMP packets -> print hopname/ip & timestamp(?)
+
+//else (port unreachable) -> end we got our host

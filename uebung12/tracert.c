7799d3aff0e4e7342ce81e5d930d5d228d0c2611
stendler <m.stendler@fu-berlin.de>
2015-01-28 18:04:35 +0100

    hostname to ip

--- uebung12/tracert.c
+++ uebung12/tracert.c
@@ -70,6 +70,33 @@ int get_local_ip ( char * buffer)
 	close(sock);
 }
 
+/*
+	Get ip from domain name
+ */
+char* hostname_to_ip(char * hostname)
+{
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
+
+	if ( (he = gethostbyname( hostname ) ) == NULL)
+	{
+		// get the host info
+		herror("gethostbyname");
+		return NULL;
+	}
+
+	addr_list = (struct in_addr **) he->h_addr_list;
+
+	for(i = 0; addr_list[i] != NULL; i++)
+	{
+		//Return the first one;
+		return inet_ntoa(*addr_list[i]) ;
+	}
+
+	return NULL;
+}
+
 struct pseudo_header    //needed for checksum calculation
 {
 	unsigned int source_address; //u_int32_t

0a5078fcd1d169975ba215ee4a62b95b999f3e70
stendler <m.stendler@fu-berlin.de>
2015-01-29 09:02:04 +0100

    edited comments

--- uebung12/tracert.c
+++ uebung12/tracert.c
@@ -33,9 +33,6 @@ Raw-Socket: Raw-Sockets sind Internet-Sockets, die nicht wie Standart-Sockets au
 ////http://www.binarytides.com/tcp-syn-portscan-in-c-with-linux-sockets/
 //https://stackoverflow.com/questions/15458438/implementing-traceroute-using-icmp-in-c
 //http://unix.superglobalmegacorp.com/Net2/newsrc/netinet/ip_icmp.h.html
-//
-
-//#define _GNU_SOURCE
 
 //includes
 #include <stdio.h>	//for printf
@@ -49,79 +46,7 @@ Raw-Socket: Raw-Sockets sind Internet-Sockets, die nicht wie Standart-Sockets au
 #include <netinet/ip_icmp.h>
 #include <string.h> //memset
 #include <arpa/inet.h>
-//#include <sys/select.h>
 #include <errno.h> //For errno - the error number
-//#include <netinet/tcp.h>	//Provides declarations for tcp header
-//source stackoverflow TODO test which headers are really needed
-//#include <unistd.h>
-//#include <sys/ipc.h>
-//#include <fcntl.h>
-//#include <sys/stat.h>
-//#include <sys/sem.h>
-//#include <signal.h>
-//#include <poll.h>
-//#include <pthread.h>
-//#include <sys/un.h>
-
-/* IP header structure *
-    struct iphdr {
-
-        unsigned char      ihl;
-        unsigned char      version;
-        unsigned char      tos;
-        unsigned short int len;
-        unsigned short int id;
-        unsigned char      flags;
-        unsigned short int offset;
-        unsigned char      ttl;
-        unsigned char      protocol;
-        unsigned short int chksum;
-        unsigned int       sourceip;
-        unsigned int       destip;
-
-      }; */
-
-/*struct icmphdr {
-	unsigned char	icmp_type;		// type of message, see below
-	unsigned char	icmp_code;		// type sub code
-	unsigned short	icmp_cksum;		// ones complement cksum of struct
-	union {
-		unsigned char ih_pptr;			// ICMP_PARAMPROB
-		struct in_addr ih_gwaddr;	// ICMP_REDIRECT
-		struct ih_idseq {
-			n_short	icd_id;
-			n_short	icd_seq;
-		} ih_idseq;
-		int ih_void;
-	} icmp_hun;
-#define	icmp_pptr	icmp_hun.ih_pptr
-#define	icmp_gwaddr	icmp_hun.ih_gwaddr
-#define	icmp_id		icmp_hun.ih_idseq.icd_id
-#define	icmp_seq	icmp_hun.ih_idseq.icd_seq
-#define	icmp_void	icmp_hun.ih_void
-	union {
-		struct id_ts {
-			n_time its_otime;
-			n_time its_rtime;
-			n_time its_ttime;
-		} id_ts;
-		struct id_ip  {
-			struct iphdr idi_ip;
-			// options and then 64 bits of data
-		} id_ip;
-		unsigned long	id_mask;
-		char	id_data[1];
-	} icmp_dun;
-#define	icmp_otime	icmp_dun.id_ts.its_otime
-#define	icmp_rtime	icmp_dun.id_ts.its_rtime
-#define	icmp_ttime	icmp_dun.id_ts.its_ttime
-#define	icmp_ip		icmp_dun.id_ip.idi_ip
-#define	icmp_mask	icmp_dun.id_mask
-#define	icmp_data	icmp_dun.id_data
-};
-*/
-
-
 
 //holt die lokale ip adresse aus einem socket paket, das nach draußen geschickt wird
 int get_local_ip ( char * buffer)
@@ -146,7 +71,6 @@ int get_local_ip ( char * buffer)
 
 	inet_ntop(AF_INET, &name.sin_addr, buffer, 100); //removed unused pointer
 
-	//close(sock);
 	return err; //err was unused
 }
 
@@ -162,7 +86,7 @@ char* hostname_to_ip(char * hostname)
 	if ( (he = gethostbyname( hostname ) ) == NULL)
 	{
 		// get the host info
-		perror("gethostbyname"); //FIXME impplicit declaration of herror
+		perror("gethostbyname");
 		return NULL;
 	}
 
@@ -211,7 +135,7 @@ int main(int argc, char *argv[])
   if(argc == 2){
 
   	//create send socket
-  	int sd = socket (PF_INET, SOCK_RAW, IPPROTO_ICMP); //IPv4,Raw-socket, IPPROTO_TCP/RAW TODO IPPROTO_RAW and we save goto1 later
+  	int sd = socket (PF_INET, SOCK_RAW, IPPROTO_ICMP); //IPv4,Raw-socket, IPPROTO_TCP/RAW
   	if(sd == -1)
   	{
     	//socket creation failed, may be because of non-root privileges
@@ -227,14 +151,6 @@ int main(int argc, char *argv[])
 			printf ("Error setting IP_HDRINCL. Error number : %d . Error message : %s \n" , errno , strerror(errno));
 			exit(0);
 		}
-
-		/* TODO: this is a container for code used in sources but I couldn't find use for yet
-
-	struct pseudo_header psh;
-
-	int source_port = 43591;
-
-		END unplaced source code container*/
 
   	//build custom headers
     	//Datagram to represent the packet
@@ -293,7 +209,7 @@ int main(int argc, char *argv[])
 			iph->daddr = dest_ip.s_addr;
 
     	//icmp
-			icmphd->type = ICMP_ECHO; // ping request FIXME undeclared
+			icmphd->type = ICMP_ECHO; // ping request
       icmphd->code = 0;					//
       icmphd->checksum = 0;
       //icmphd->un.echo.id = 0;
@@ -314,27 +230,27 @@ int main(int argc, char *argv[])
 			while(ttl <= 255){
         ttl++;
         //DEBUG
-        printf("--(1)ttl++\n");
+        //printf("--(1)ttl++\n");
 				//set ttl in ip header
 				iph->ttl = ttl;
 				//icmphd->un.echo.sequence = ttl;
 				//package / checksum? &more
-        iph->check = csum ((unsigned short *) datagram, iph->tot_len >> 1); //TODO put this never - maybe the kernel does this for us?
+        iph->check = csum ((unsigned short *) datagram, iph->tot_len >> 1);
 				icmphd->checksum = csum((unsigned short *) (datagram + 20), 4);
 				//DEBUG
-				printf("--(2) checksum\n");
-				//TODO multiple simultanously ? --> for loop which does'nt incr ttl but sends 3 packages
+				//printf("--(2) checksum\n");
+				//TODO multiple simultanously ? --> for-loop which doesn't incr ttl but sends 3 packages
 					//send custom ip header with icmp header in body and ttl
-					sendto (sd, datagram, sizeof(struct iphdr) + sizeof(struct icmphdr), 0, (struct sockaddr *)&dest, sizeof dest); //TODO is datagram really complete
+					sendto (sd, datagram, sizeof(struct iphdr) + sizeof(struct icmphdr), 0, (struct sockaddr *)&dest, sizeof dest);
     			//receive ICMP packets
 					char rcvbuffer[4096] = { 0 };
 					//DEBUG
-					printf("--(3)package send\n");
+					//printf("--(3)package send\n");
 					recvfrom (sd, rcvbuffer, sizeof(struct iphdr) + sizeof(struct icmphdr), 0, (struct sockaddr *) &listener, &saddr_len);
 					struct icmphdr *icmphd2 = (struct icmphdr *) (rcvbuffer + 20);
 					//print [ttl] hop_ip (TODO resolve) [timestamp/delay]
-					printf("[%d] %s\n",ttl,inet_ntoa(listener.sin_addr));
-					if (icmphd2->type == 0){
+					printf("[%d] %s\n",ttl,inet_ntoa(listener.sin_addr)); //TODO  sind das ueberhaupt meine icmp's --> check that plox
+					if (icmphd2->type == 0){ //TODO weitere Bedingungen, e.g.ist der gewuenschte host im header?
 						printf("\n Destination reached!\n");
 						return 0;
 					}
